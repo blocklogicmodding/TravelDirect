ce4651dba7af6b3d0ed548d1f45ec39a289ea120
deonjonker123 <deon.jonker@hotmail.com>
2025-04-08 22:11:04 +0200

    Initial mod setup. Blocks, items, textures, recipes, and loot tables added

---
+++ src/main/java/com/blocklogic/traveldirect/block/custom/NetherAnchorBlock.java
@@ -0,0 +1,9 @@
+package com.blocklogic.traveldirect.block.custom;
+
+import net.minecraft.world.level.block.Block;
+
+public class NetherAnchorBlock extends Block {
+    public NetherAnchorBlock(Properties properties) {
+        super(properties);
+    }
+}
---
+++ src/main/java/com/blocklogic/traveldirect/item/custom/NetherActivatorItem.java
@@ -0,0 +1,17 @@
+package com.blocklogic.traveldirect.item.custom;
+
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+
+public class NetherActivatorItem extends Item {
+
+    public NetherActivatorItem(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        return super.useOn(context);
+    }
+}
